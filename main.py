2c90bed3ee588885d4f4acd4048e25087e3b6298
ASardor242 <132080337+ASardor242@users.noreply.github.com>
2023-04-28 19:59:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel,
                              QHBoxLayout, QVBoxLayout, QListWidget, QFileDialog)
+app = QApplication([])
+main_win = QWidget()
+main_win.resize(700, 500)
+main_win.setWindowTitle("Easy Editor")
+btn_dir = QPushButton('Папка')
+list_images = QListWidget()
+label_image = QLabel('Картинка')
 
+btn_left = QPushButton('Лево')
+btn_right = QPushButton('Право')
